7cd1a5d49e386419b4543e527c3e444489b831b6
Salahuddin <P70074460@unimaas.nl>
2023-07-03 17:57:54 +0200

    Update main_g.py

--- main_g.py
+++ main_g.py
@@ -1024,16 +1024,17 @@ def add_bg_from_local(image_file):
     )
 if __name__ == "__main__":
     
-    if "firebase" not in st.session_state:
-        st.session_state.firebase = True
-        # Initialize Firebase Admin SDK
 
-    if not firebase_admin._apps:
+
+    if not st.session_state.firebase:
         cred = credentials.Certificate('firestore-key.json')
         firebase_admin.initialize_app(cred, name="st-trial")
         
     st.session_state["db"] = firestore.client()
     
+    if "firebase" not in st.session_state:
+        st.session_state.firebase = True
+        # Initialize Firebase Admin SDK
 
     st.set_page_config(page_title='In silico Trial', layout="wide")
 
